f60680ab248cfee100197d164965139dbf246e51
priscila Duarte <priscilauchoaa@gmail.com>
2019-02-16 16:00:50 -0200

    Criar matriz para guardar jogadas

--- script.js
+++ script.js
@@ -8,26 +8,60 @@ const posicaoC1 = document.querySelector('.c1');
 const posicaoC2 = document.querySelector('.c2');
 const posicaoC3 = document.querySelector('.c3');
 
-const jogadorX = document.querySelector('.visivel-jogador');
-const maquinaO = document.querySelector('.visivel-maquina');
+let vezDo = 'xis';
+let matriz = [
+    [null,null,null],
+    [null,null,null], 
+    [null,null,null]
+];
 
-const jogada = function(){
+function verificaJogo() {
 
-    posicaoA1.classList.toggle('visivel-jogador');
+    // escrever a logica da verificacao do jogo utilizando a matriz
 }
 
-posicaoA1.onclick = function() {
-    jogada();
-    posicaoA1.onclick = null;
-};
+function preencheMatriz(identificador, simbolo) {
 
-
-const jogada2 = function(){
-    posicaoA2.classList.toggle('visivel-jogador');
+    // poderia ser um monte de if e else if no lugar do switch
+    switch(identificador) {
+        case 'a1': matriz[0][0] = simbolo; break;
+        case 'a2': matriz[0][1] = simbolo; break;
+        case 'a3': matriz[0][2] = simbolo; break;
+        case 'b1': matriz[1][0] = simbolo; break;
+        case 'b2': matriz[1][1] = simbolo; break;
+        case 'b3': matriz[1][2] = simbolo; break;
+        case 'c1': matriz[2][0] = simbolo; break;
+        case 'c2': matriz[2][1] = simbolo; break;
+        case 'c3': matriz[2][2] = simbolo; break;
+    }
 }
 
-posicaoA2.onclick = function() {
-    jogada2();
-    posicaoA2.onclick = null;
+function realizaJogada(elemento) {
+
+
+    // pegar identificador do item clicado e preenche a matriz no javascript
+    preencheMatriz(elemento.srcElement.className, vezDo); // ex: b1, c3, a2
+
+    // coloca no grid xis ou bola
+    elemento.srcElement.classList.toggle(vezDo);
+    elemento.srcElement.onclick = null;
+
+    if (vezDo === 'xis') {
+        vezDo = 'bola';
+    } else {
+        vezDo = 'xis';
+    }
+
+    verificaJogo();
 };
+
+posicaoA1.onclick = realizaJogada;
+posicaoA2.onclick = realizaJogada;
+posicaoA3.onclick = realizaJogada;
+posicaoB1.onclick = realizaJogada;
+posicaoB2.onclick = realizaJogada;
+posicaoB3.onclick = realizaJogada;
+posicaoC1.onclick = realizaJogada;
+posicaoC2.onclick = realizaJogada;
+posicaoC3.onclick = realizaJogada;
 
